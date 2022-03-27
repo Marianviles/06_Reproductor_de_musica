8bf93dbd554068a0889e297e043146cd22c842c9
Marian Hernandez Aviles <160642@mail.unacar.mx>
2022-03-27 12:23:47 -0600

    first commit

---
+++ playlist.js
@@ -0,0 +1,105 @@
+
+let allMusic = [
+    {
+      name: "Days",
+      artist: "The Drums",
+      img: "days",
+      src: "days"
+    },
+    {
+      name: "After Dark",
+      artist: "Mr.Kitty",
+      img: "after dark",
+      src: "after dark"
+    },
+    {
+      name: "Cigarettes out the Windows",
+      artist: "TV Girl",
+      img: "cigaretes",
+      src: "cigaretes"
+    },
+    {
+      name: "Island",
+      artist: "The xx",
+      img: "island",
+      src: "island"
+    },
+    {
+      name: "Money",
+      artist: "The Drums",
+      img: "money",
+      src: "money"
+    },
+    {
+      name: "Rebel Rebel",
+      artist: "David Bowie",
+      img: "rebel rebel",
+      src: "rebel rebel"
+    },
+
+    {
+      name: "Clint Eastwood",
+      artist: "Gorillaz",
+      img: "gorillaz",
+      src: "gorillaz"
+    },
+
+    {
+      name: "I Know is Over",
+      artist: "The Smiths",
+      img: "i know is over",
+      src: "i know is over"
+    },
+
+    {
+      name: "Lithium",
+      artist: "Nirvana",
+      img: "lithium",
+      src: "lithium"
+    },
+
+    {
+      name: "Lovesong",
+      artist: "The Cure",
+      img: "lovesong",
+      src: "lovesong"
+    },
+
+    {
+      name: "Pink in the Night",
+      artist: "Mitski",
+      img: "pink in the night",
+      src: "pink in the night"
+    },
+
+    {
+      name: "Reptilia",
+      artist: "The Strokes",
+      img: "reptilia",
+      src: "reptilia"
+    },
+
+    {
+      name: "The Perfect Girl",
+      artist: "Mareux",
+      img: "the perfect girl",
+      src: "the perfect girl"
+    },
+
+    {
+      name: "Well I Wonder",
+      artist: "The Smiths",
+      img: "well i wonder",
+      src: "well i wonder"
+    },
+
+    {
+      name: "Not Allowed",
+      artist: "TV Girl",
+      img: "cigaretes",
+      src: "cigaretes"
+    },
+
+
+   
+  ];
